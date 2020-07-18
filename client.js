4f81bb850e20f80b0969b5c79b12a797141c34e1
Guy Grinberg <Guy.Grinberg@skyboxsecurity.com>
2020-07-18 15:45:01 +0300

    Module 4

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios").default
 const { randomString, timeout } = require("./utils")
+const { response } = require("express")
+
 
 const config = {
 	port: 9000,
@@ -26,6 +28,43 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+
+app.get('/authorize', (req,res)=>{
+	 state = randomString();
+	 const myURL = new URL(config.authorizationEndpoint);
+	 myURL.searchParams.set("response_type" ,"code");
+	 myURL.searchParams.set("client_id" , config.clientId );
+	 myURL.searchParams.set("redirect_uri",config.redirectUri );
+	 myURL.searchParams.set("scope","permission:name permission:date_of_birth");
+	 myURL.searchParams.set("state", state);
+	 res.redirect(myURL);
+})
+
+app.get('/callback',(req,res)=>{
+	const stateReq = req.query.state;
+	if(state!== stateReq )
+	{
+		res.status(403).end();
+		return
+	}
+
+	axios({
+		method: 'post',
+		url: config.tokenEndpoint,
+		auth: {username : config.clientId , password:config.clientSecret},
+		data: {
+			code : req.query.code
+		}
+	  }).then(response =>{
+		axios({
+			method:'get',
+			url: config.userInfoEndpoint,
+			headers: {authorization : "bearer " +response.data.access_token}
+		}).then(response =>{
+			res.render("welcome",{user:response.data});
+		})
+	  })
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
